fa32097fe6852535d981ef1a0c4e1410b394e970
Jonathan Pyers <jpyers@headspin.io>
2022-07-05 22:32:05 -0400

    Remove unused helpers.

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,4 +1,4 @@
 export { default as log, LogLevel } from "./logger";
 export { getUuid } from "./uuid";
-export { sleep, bool, int } from "./utils";
+export { sleep } from "./utils";
 export { default as duration } from "./duration";
