f1554b122b3b0c16cfa5a2855b89c33f769b3acc
Jonathan Pyers <jpyers@headspin.io>
2022-07-05 22:22:27 -0400

    The provided filename is already an absolute path.

--- src/internal/loader/main.ts
+++ src/internal/loader/main.ts
@@ -1,15 +1,10 @@
-import path from "node:path";
-
 import logger, { LogLevel } from "../../shared/logger";
 
 
 export default async function loadModule<ModuleExports>(filename: string, defaultMiddleware = {}): Promise<Readonly<ModuleExports>> {
-  const fromProjectRoot = path.join("/", filename).slice(1);
-  const absolute = path.resolve(fromProjectRoot);
+  logger({ level: LogLevel.DEBUG, scope: "loader" }, "import", filename);
 
-  logger({ level: LogLevel.DEBUG, scope: "loader" }, "import", fromProjectRoot);
-
-  const module = await import(absolute);
+  const module = await import(filename);
 
   return module;
 }
