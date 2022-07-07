664c773ed4007f6fb8a9de6855da513fef782200
Jonathan Pyers <jpyers@headspin.io>
2022-07-07 19:00:27 -0400

    Export `Connection`.

--- src/internal/ws/index.ts
+++ src/internal/ws/index.ts
@@ -1,5 +1,7 @@
 import { WSContext } from "./server";
 import { WebSocketTokenServer } from "./server";
+import { Connection } from "./server";
 
 export { WSContext as Context };
 export { WebSocketTokenServer };
+export { Connection };
